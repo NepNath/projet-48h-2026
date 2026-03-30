b752e9d60de5b96c40f64eb3bd344ac9081bd639
Antoine Mathié <antoine.mathie100@gmail.com>
2026-03-30 17:09:27 +0200

    fix: changed the name of the scriptable object

--- Assets/Scripts/SpeedBalatro/SpeedBalatroCardFactory.cs
+++ Assets/Scripts/SpeedBalatro/SpeedBalatroCardFactory.cs
@@ -1,65 +1,69 @@
 using UnityEngine;
 
-[System.Serializable]
-public enum CardSuit
+namespace SpeedBalatro
 {
-    Hearts,
-    Diamonds, 
-    Clubs,
-    Spades
-}
 
-[System.Serializable]
-public enum CardRank
-{
-    Ace = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 11,
-    Queen = 12,
-    King = 13
-}
+    [System.Serializable]
+    public enum CardSuit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
 
-[System.Serializable]
-public enum CardEffect
-{
-    None,
-    Double,
-    Triple,
-    AddBonus,
-    Multiply,
-    WildCard
-}
+    [System.Serializable]
+    public enum CardRank
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
 
-[CreateAssetMenu(fileName = "New Card", menuName = "Mini-game/SpeedBalatroCardFactory", order = 1)]
-public class Card : ScriptableObject
-{
-    [Header("Card Identity")]
-    public string cardName;
-    public Sprite cardSprite;
-    public int cardId;
+    [System.Serializable]
+    public enum CardEffect
+    {
+        None,
+        Double,
+        Triple,
+        AddBonus,
+        Multiply,
+        WildCard
+    }
 
-    [Header("Card Attributes")]
-    public CardSuit suit;
-    public CardRank rank;
-    public float baseValue;
+    [CreateAssetMenu(fileName = "New Card", menuName = "Mini-game/Speed Balatro Card", order = 1)]
+    public class Card : ScriptableObject
+    {
+        [Header("Card Identity")]
+        public string cardName;
+        public Sprite cardSprite;
+        public int cardId;
 
-    [Header("Card Effects")]
-    public bool isSpecialCard;
-    public CardEffect effect;
-    public float effectMultiplier = 1;
+        [Header("Card Attributes")]
+        public CardSuit suit;
+        public CardRank rank;
+        public float baseValue;
 
-    [Header("UI")]
-    public Color cardColor = Color.white;
+        [Header("Card Effects")]
+        public bool isSpecialCard;
+        public CardEffect effect;
+        public float effectMultiplier = 1;
 
-    public float GetValue() => baseValue * effectMultiplier;
-    public bool IsRed() => suit == CardSuit.Hearts || suit == CardSuit.Diamonds;
-    public bool IsBlack() => suit == CardSuit.Clubs || suit == CardSuit.Spades;
+        [Header("UI")]
+        public Color cardColor = Color.white;
+
+        public float GetValue() => baseValue * effectMultiplier;
+        public bool IsRed() => suit == CardSuit.Hearts || suit == CardSuit.Diamonds;
+        public bool IsBlack() => suit == CardSuit.Clubs || suit == CardSuit.Spades;
+    }
 }

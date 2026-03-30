e8699e80472af83b502d07f11037976b97ef72de
Antoine Mathié <antoine.mathie100@gmail.com>
2026-03-30 16:09:29 +0200

    speedbalatro base

---
+++ Assets/Scripts/SpeedBalatro/SpeedBalatro.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedBalatro : MonoBehaviour
+{
+    private Dictionary<string, Image> _speedBalatroDeck = new();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

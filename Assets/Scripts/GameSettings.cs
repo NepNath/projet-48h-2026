90a70f38eede3e51dba631ce8a2579a130b181f7
NepNath <neptonath@gmail.com>
2026-03-30 18:25:01 +0200

    0.1.0 - main - adding GameSettings script for constant variables

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    
+    //To use as yieldReturnNewWaitForSeconds in minigame logic scripts
+    public static int QuestionTimer = 15;
+    public static int MiniGameTimer = 7;
+}

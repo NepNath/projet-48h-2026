d1f85b1bd9d6d4d1f3b5a8fd6d7cabd21dc71394
NepNath <neptonath@gmail.com>
2026-03-31 14:00:18 +0200

    URGENT

--- Assets/Scripts/Core/SceneFlow.cs
+++ Assets/Scripts/Core/SceneFlow.cs
@@ -6,7 +6,7 @@ public static class SceneFlow
     public const string QuestionScene = "Question";
     public const string GameOverScene = "Perdu";
 
-    static readonly string[] MiniGameScenes = { "KeyCard", "DigitCode", "Apple", "cable_game",  };
+    static readonly string[] MiniGameScenes = { "KeyCard", "DigitCode", "Apple", "cable_game", "Chocolate" };
     static int nextMiniGameIndex;
 
     public static void ResetRun()

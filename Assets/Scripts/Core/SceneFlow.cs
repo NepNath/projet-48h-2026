5dafe7f836d62c495f99cdcad235e910292f8439
Paolo Antonini <pantonini64@gmail.com>
2026-03-31 09:59:27 +0200

    fix: update game flow - questions only after losing mini-game

--- Assets/Scripts/Core/SceneFlow.cs
+++ Assets/Scripts/Core/SceneFlow.cs
@@ -29,7 +29,7 @@ public static class SceneFlow
         else
         {
             HealthManager.Instance?.LoseLife();
-            return QuestionScene;
+            return PickMiniGame();
         }
     }
 
@@ -37,7 +37,7 @@ public static class SceneFlow
     {
         if (won)
         {
-            return QuestionScene;
+            return PickMiniGame();
         }
         else
         {
